2db4831d49dc5c691ce7846d64d18d8e11e305db
Chandramouleswaran <chandramouleswaran@gmail.com>
2013-09-15 22:14:30 -0500

    Clean start

---
+++ Src/Hypertest.Core/TestCase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Practices.Unity;
+
+namespace Hypertest.Core
+{
+    /// <summary>
+    /// The basic unit of a test case in the Hypertest framework
+    /// </summary>
+    public abstract class TestCase : ICloneable
+    {
+        #region Virtuals
+        public virtual void Setup(){}
+        public virtual void Body(){}
+        public virtual void Cleanup(Exception e = null)
+        {
+            //Need to access logger
+        }
+        #endregion
+
+        public void Run()
+        {
+            try
+            {
+                InitRun();
+                Setup();
+                Body();
+                FinalizeRun();
+                Cleanup();
+            }
+            catch (Exception e)
+            {
+                Cleanup(e);
+            }
+        }
+
+        [Dependency("UnityContainer")]
+        protected IUnityContainer Container { get; set; }
+
+        public object Clone()
+        {
+            TestCase result = null;
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                BinaryFormatter binFormatter = new BinaryFormatter();
+                binFormatter.Serialize(memStream, this);
+                memStream.Seek(0, SeekOrigin.Begin);
+                result = binFormatter.Deserialize(memStream) as TestCase;
+                memStream.Close();
+            }
+            return result;
+        }
+
+        private void InitRun()
+        {
+            //Set the actual result to None
+        }
+        
+        private void FinalizeRun()
+        {
+            //This is where we want to look at the properties and assign it to variables
+        }
+    }
+}
--- Src/Hypertest.Core/TestScenario.cs
+++ Src/Hypertest.Core/TestScenario.cs
@@ -1,74 +1,14 @@
-﻿/*
-    Hypertest - A web testing framework using Selenium
-    Copyright (C) 2012  Chandramouleswaran Ravichandran
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-using System;
-using System.Collections.ObjectModel;
-
-using Hypertest.Core.Utils;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Hypertest.Core
 {
     /// <summary>
-    /// TODO: Update summary.
+    /// The basic unit of a test scenario in the Hypertest framework
     /// </summary>
-    [Serializable]
-    [TestIgnore]
-    public class TestScenario : TestCase
+    public class TestScenario
     {
-        #region CTOR
-        public TestScenario()
-        {
-            this.BaseURL = "http://www.google.com";
-            this.BrowserType = BrowserType.Chrome;
-            this.Variables = new ObservableCollection<Variable>();
-        }
-        #endregion
-        
-        #region Property
-        public override bool IsParent
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public ScenarioType ScenarioType { get; set; }
-
-        public BrowserType BrowserType { get; set; }
-
-        public String BaseURL { get; set; }
-
-        public ObservableCollection<Variable> Variables { get; set; }
-
-        #endregion
-
-        #region Methods
-        public override void Initialize(ITestRunner runner)
-        {
-            this.Description = "TestScenario";
-            if (this.ScenarioType == ScenarioType.Web)
-            {
-                runner.Create(this.BrowserType);
-                runner.Driver.Navigate().GoToUrl(this.BaseURL);
-            }
-        }
-
-        #endregion
     }
 }
