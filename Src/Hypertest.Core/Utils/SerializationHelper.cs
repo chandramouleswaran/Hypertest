ba050c29a7b752f0ec11cefbdf10d93e82719de4
Chandramouleswaran <chandramouleswaran@gmail.com>
2013-10-10 18:50:26 -0500

    License

--- Src/Hypertest.Core/Utils/SerializationHelper.cs
+++ Src/Hypertest.Core/Utils/SerializationHelper.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿#region License
+
+// Copyright (c) 2013 Chandramouleswaran Ravichandran
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
@@ -14,7 +26,7 @@ namespace Hypertest.Core.Utils
 
             using (MemoryStream ms = new MemoryStream())
             {
-                DataContractSerializer dcs = new DataContractSerializer(typeof(T), extraTypes);
+                DataContractSerializer dcs = new DataContractSerializer(typeof (T), extraTypes);
                 dcs.WriteObject(ms, obj);
                 return ms.ToArray();
             }
@@ -30,8 +42,8 @@ namespace Hypertest.Core.Utils
                 ms.Write(data, 0, data.Length);
                 ms.Seek(0, 0);
 
-                DataContractSerializer dcs = new DataContractSerializer(typeof(T), extraTypes);
-                return (T)dcs.ReadObject(ms);
+                DataContractSerializer dcs = new DataContractSerializer(typeof (T), extraTypes);
+                return (T) dcs.ReadObject(ms);
             }
         }
     }
