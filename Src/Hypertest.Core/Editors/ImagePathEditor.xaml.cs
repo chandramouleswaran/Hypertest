8a997b0393ba198b35ac2553386821cbd501c326
Chandramouleswaran Ravichandran <chandramouleswaran@gmail.com>
2014-02-02 13:28:21 -0600

    Sreenshot display

---
+++ Src/Hypertest.Core/Editors/ImagePathEditor.xaml.cs
@@ -0,0 +1,56 @@
+﻿#region License
+
+// Copyright (c) 2014 Chandramouleswaran Ravichandran
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Forms;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+using Binding = System.Windows.Data.Binding;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace Hypertest.Core.Editors
+{
+	/// <summary>
+	/// Interaction logic for ImagePathEditor.xaml
+	/// </summary>
+	public partial class ImagePathEditor : UserControl, ITypeEditor
+	{
+		#region CTOR
+		public ImagePathEditor()
+		{
+			InitializeComponent();
+		}
+		#endregion
+
+		#region Dependency Property
+		public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(string), typeof(ImagePathEditor), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+		public string Value
+		{
+			get { return (string)GetValue(ValueProperty); }
+			set { SetValue(ValueProperty, value); }
+		} 
+		#endregion
+
+		#region ITypeEditor
+		public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+		{
+			Binding binding = new Binding("Value");
+			binding.Source = propertyItem;
+			binding.Mode = propertyItem.IsReadOnly ? BindingMode.OneWay : BindingMode.TwoWay;
+			BindingOperations.SetBinding(this, ImagePathEditor.ValueProperty, binding);
+			return this;
+		} 
+		#endregion
+	}
+}
